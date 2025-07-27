a48a1dc204c9cc34d0f40927c0dd7dbc4be647ff
Dawy007 <ieldaw394@gmail.com>
2025-07-27 04:15:13 +0300

    Create 1.7.c

---
+++ Unit 1/Assgnments/Assignment1/1.7.c
@@ -0,0 +1,17 @@
+int main() {
+    int x,y;
+    printf("Enter Value of a: ");
+    fflush(stdin);
+    fflush(stdout);
+    scanf("%d", &x);
+    printf("Enter Value of b: ");
+    fflush(stdin);
+    fflush(stdout);
+    scanf("%d", &y);
+    x = x * y;
+    y = x / y;
+    x = x / y;
+    printf("After Swapping, value of a = %d\r\n", x);
+    printf("After Swapping, value of b = %d", y);
+    return 0;
+}
