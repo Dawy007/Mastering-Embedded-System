2d2488638ac303890f64d2deb05343c8bcbb13bd
Dawy007 <ieldaw394@gmail.com>
2025-07-27 03:55:48 +0300

    Update 1.2.c

--- Unit 1/Assgnments/Assignment1/1.2.c
+++ Unit 1/Assgnments/Assignment1/1.2.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main() {
+    int x;
+    printf("Enter n integer: ");
+    fflush(stdin);
+    fflush(stdout);
+    scanf("%d", &x);
+    printf("You Entered: %d", x);
+    return 0;
+}
