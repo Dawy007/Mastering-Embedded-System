61dbc180612bbb1a9b6c2795e3f6ec76834cf6b1
Dawy007 <ieldaw394@gmail.com>
2025-07-27 05:43:52 +0300

    Update 1.6.c

--- Unit 1/Assgnments/Assignment1/1.6.c
+++ Unit 1/Assgnments/Assignment1/1.6.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
-int main() {
-    int x,y,temp;
+int main()
+{
+    int x, y, temp;
     printf("Enter Value of a: ");
-    fflush(stdin);
     fflush(stdout);
     scanf("%d", &x);
     printf("Enter Value of b: ");
-    fflush(stdin);
     fflush(stdout);
     scanf("%d", &y);
     temp = x;
-    x=y;
-    y=temp;
+    x = y;
+    y = temp;
     printf("After Swapping, value of a = %d\r\n", x);
     printf("After Swapping, value of b = %d", y);
     return 0;
