830d4f0f06ad3597347c213d3a4514418edeece7
Dawy007 <ieldaw394@gmail.com>
2025-07-27 05:43:19 +0300

    Update 1.3.c

--- Unit 1/Assgnments/Assignment1/1.3.c
+++ Unit 1/Assgnments/Assignment1/1.3.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int main() {
-    int x,y,sum;
+int main()
+{
+    int x, y, sum;
     printf("Entyer Two integers: ");
-    fflush(stdin);
     fflush(stdout);
-    scanf("%d %d", &x,&y);
+    scanf("%d %d", &x, &y);
     sum = x + y;
     printf("Sum: %d", sum);
     return 0;
