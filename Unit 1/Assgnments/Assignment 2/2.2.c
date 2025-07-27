59f8c55dc2e0ef87e21b7a4b0f5ffa73287f51fe
Dawy007 <ieldaw394@gmail.com>
2025-07-27 04:29:40 +0300

    Create 2.2.c

---
+++ Unit 1/Assgnments/Assignment 2/2.2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+int main() {
+    char x,lower;
+    printf("Enter an alphabet: ");
+    fflush(stdin);
+    fflush(stdout);
+    scanf(" %c", &x);
+    lower = tolower(x);
+    if ('a' == lower || 'e' == lower || 'i' == lower || 'o' == lower || 'u' == lower)
+    printf("%c is vowel", x);
+    else 
+    printf("%c is consonant", x);
+    return 0;
+}
