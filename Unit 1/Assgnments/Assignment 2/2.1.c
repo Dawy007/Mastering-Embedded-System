2434bcd86417d5c66b1b3e30f6f0510b25835bc9
Dawy007 <ieldaw394@gmail.com>
2025-07-27 05:42:03 +0300

    Update 2.1.c

--- Unit 1/Assgnments/Assignment 2/2.1.c
+++ Unit 1/Assgnments/Assignment 2/2.1.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-int main() {
+int main()
+{
     int x;
     printf("Enter an integer you want to check: ");
-    fflush(stdin);
     fflush(stdout);
     scanf("%d", &x);
     if (0 == x % 2)
-    printf("%d is Even", x);
-    else 
-    printf("%d is Odd", x);
+        printf("%d is Even", x);
+    else
+        printf("%d is Odd", x);
     return 0;
 }
