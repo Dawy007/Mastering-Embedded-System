50dfd16fc85002136eeb78473e290088e005898b
Dawy007 <ieldaw394@gmail.com>
2025-07-27 05:41:35 +0300

    Update 2.3.c

--- Unit 1/Assgnments/Assignment 2/2.3.c
+++ Unit 1/Assgnments/Assignment 2/2.3.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
-int main() {
-    int x,y,z;
+int main()
+{
+    int x, y, z;
     printf("Enter Three Numbers: ");
-    fflush(stdin);
     fflush(stdout);
     scanf("%d %d %d", &x, &y, &z);
-    if (x>=y){
-        if (x>=z)
-        printf("Largest Number = %d",x);
+    if (x >= y)
+    {
+        if (x >= z)
+            printf("Largest Number = %d", x);
         else
-        printf("Largest Number = %d",z);
+            printf("Largest Number = %d", z);
     }
-    else{
-        if (y>=z)
-        printf("Largest Number = %d",y);
+    else
+    {
+        if (y >= z)
+            printf("Largest Number = %d", y);
         else
-        printf("Largest Number = %d",z);  
+            printf("Largest Number = %d", z);
     }
     return 0;
 }
