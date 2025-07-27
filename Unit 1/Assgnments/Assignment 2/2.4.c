0039fa1d927e0a7ba844f778ba39ed2ebddb7037
Dawy007 <ieldaw394@gmail.com>
2025-07-27 05:36:53 +0300

    Update 2.4.c

--- Unit 1/Assgnments/Assignment 2/2.4.c
+++ Unit 1/Assgnments/Assignment 2/2.4.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
-int main() {
+int main()
+{
     float x;
     printf("Enter a Number: ");
-    fflush(stdin);
     fflush(stdout);
     scanf(" %f", &x);
-    if (0 > x){
-        printf("%f is Negative", x);
+    if (0 > x)
+    {
+        printf("%.1f is Negative", x);
     }
-    else if (0 < x){
-        printf("%f is Positive", x);
+    else if (0 < x)
+    {
+        printf("%.1f is Positive", x);
     }
-    else  printf("You Entered Zero");
+    else
+        printf("You Entered Zero");
     return 0;
 }
