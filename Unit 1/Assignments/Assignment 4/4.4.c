b127e2ed51f8c7e48fc359a4f0f07d76cfd9ac1a
Dawy007 <ieldaw394@gmail.com>
2025-07-30 05:25:13 +0300

    Add Hello.c

--- Unit 1/Assignments/Assignment 4/4.4.c
+++ Unit 1/Assignments/Assignment 4/4.4.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
-int main() {
-    
+int base, exponent;
+
+long long power(int b, int e);
+
+int main()
+{
+    printf("Enter base and exponent: ");
+    scanf("%d %d", &base, &exponent);
+    printf("%d^%d = %lld\n", base, exponent, power(base, exponent));
     return 0;
+}
+
+long long power(int b, int e)
+{
+    if (e == 0)
+        return 1;
+    else
+        return b * power(b, e - 1);
 }
